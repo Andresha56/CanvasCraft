dd8e106d33c312bfadea2450733aa37efa822028
Andresha56 <rinkyshaw65479@gmail.com>
2024-03-31 18:01:45 +0530

    provided some more oprions to toolbar

--- client/src/Components/TextEditor/TextEditor.jsx
+++ client/src/Components/TextEditor/TextEditor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import 'react-quill/dist/quill.snow.css'; // Import CSS for the Snow theme
+import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import "./TextEditor.css"
 function TextEditor() {
@@ -11,10 +11,16 @@ function TextEditor() {
                 value={editorValue}
                 modules={{
                     toolbar: [
-                        [{ header: [1, 2, false] }],
-                        ['bold', 'italic', 'underline'],
+                        [{ 'header': [1, 2, 3, 4, 5, 6, false] },],
+                        [{ 'font': [] }],
+                        ['bold', 'italic', 'underline','strike'],
                         ['image', 'code-block'],
-                        [{ 'color': [] }],
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+                        [{ 'color': [] }, { 'background': [] }],
+                        [{ 'script': 'sub'}, { 'script': 'super' }],   
+                        [{ 'align': [] }],
+                        [{ 'indent': '-1'}, { 'indent': '+1' }], 
+                       
                     ]
                 }}
                 theme="snow"
